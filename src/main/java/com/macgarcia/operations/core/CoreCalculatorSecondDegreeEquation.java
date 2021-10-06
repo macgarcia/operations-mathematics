7d4d85e97c64f25ff25c710e2cad732632a4b469
macgarcia <marquim.iron@gmail.com>
2021-10-06 14:43:37 -0300

    Bhaskara

--- src/main/java/com/macgarcia/operations/core/CoreCalculatorSecondDegreeEquation.java
+++ src/main/java/com/macgarcia/operations/core/CoreCalculatorSecondDegreeEquation.java
@@ -14,10 +14,36 @@ public class CoreCalculatorSecondDegreeEquation extends OperationsExtra {
 		double delta = this.delta(dto);
 		
 		if (delta >= 0) {
+			
 			result.put(index++, "===================");
 			this.bhaskara(dto, delta);
+			
+			result.put(index++, "===================");
+			this.vertices(dto, delta);
 		}
 		return ResponseEntity.ok(result);
+	}
+
+	private double delta(final SecondDegreeEquationDtoIn dto) {
+		
+		startStruct();
+		
+		final double bSquare = (double) (dto.getB() * dto.getB());
+		final double minusFour = (double) - (4 * dto.getA() * dto.getC());
+		final double delta = (double) bSquare - (4 * dto.getA() * dto.getC());
+		
+		result.put(index++, "Y(x) = ax² + bx + c = 0");
+		result.put(index++, "Y(x) = " + sinal(dto.getA()) + dto.getA() + "x²"
+				+ sinal(dto.getB()) + dto.getB() + "x " + sinal(dto.getC()) + dto.getC() + " = 0");
+		
+		result.put(index++, "===================");
+		
+		result.put(index++, "∆ = b² - 4 * a * c");
+		result.put(index++, "∆ = (" + dto.getB() + ")² - 4 * " + dto.getA() + " * " + dto.getC());
+		result.put(index++, "∆ = " + bSquare + sinal(minusFour) + minusFour);
+		result.put(index++, "∆ = " + delta);
+		
+		return delta;
 	}
 
 	private void bhaskara(final SecondDegreeEquationDtoIn dto, final double delta) {
@@ -50,24 +76,25 @@ public class CoreCalculatorSecondDegreeEquation extends OperationsExtra {
 		result.put(index++, "X'' = (" + sinal(dto.getB() * -1) + dto.getB() * -1 + " - " + raizDelta + " ) / " 
 				+ twoTimesA);
 		result.put(index++, "X'' = " + xTwo + " / " + twoTimesA);
-		result.put(index++, "X'' = " + raizNegative);
-		
+		result.put(index++, "X'' = " + raizNegative);		
 	}
-
-	private double delta(final SecondDegreeEquationDtoIn dto) {
+	
+	private void vertices(final SecondDegreeEquationDtoIn dto, final double delta) {
 		
-		startStruct();
+		final double xVertice = (double) (dto.getB() * -1) / (2 * dto.getA());
+		final double yVertice = (double) (delta * -1) / (4 * dto.getA());
 		
-		final double bSquare = (double) (dto.getB() * dto.getB());
-		final double minusFour = (double) - (4 * dto.getA() * dto.getC());
-		final double delta = (double) bSquare - (4 * dto.getA() * dto.getC());
+		result.put(index++, "Xv = -b / 2 * a");
+		result.put(index++, "Xv = " + sinal(dto.getB() * -1) + (dto.getB() * -1) + " / 2 * " + dto.getA()); 
+		result.put(index++, "Xv = " + sinal(dto.getB() * -1) + (dto.getB() * -1) + " / " + (2 * dto.getA()));
+		result.put(index++, "Xv = " + xVertice);
 		
-		result.put(index++, "∆ = b² - 4 * a * c");
-		result.put(index++, "∆ = (" + dto.getB() + ")² - 4 * " + dto.getA() + " * " + dto.getC());
-		result.put(index++, "∆ = " + bSquare + sinal(minusFour) + minusFour);
-		result.put(index++, "∆ = " + delta);
+		result.put(index++, "===================");
 		
-		return delta;
+		result.put(index++, "Yv = -∆ / 4 * a");
+		result.put(index++, "Yv = " + sinal(delta * -1) + delta * -1 + " / 4 * " + dto.getA());
+		result.put(index++, "Yv = " + sinal(delta * -1) + delta * -1 + " / " + (4 * dto.getA()));
+		result.put(index++, "Yv = " + yVertice);
 	}
 
 }
